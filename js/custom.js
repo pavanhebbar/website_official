db209b8b229cf2456141a6f27c0b6e4a81e6c362
Pavan Hebbar <rpavanhebbar1996@gmail.com>
2018-09-06 08:24:41 -0600

    Got a method

--- js/custom.js
+++ js/custom.js
@@ -9,6 +9,22 @@ if (index > 0) {
 $.fn.exists = function () {
     return this.length > 0 ? this : false;
 }
+	//Handle window resize
+	var resizeFlag=0;  
+	$(window).resize(function(){
+	    resizeFlag=1;
+	})
+	checkSizeChange();
+	function checkSizeChange(){
+	    if (resizeFlag) {
+	      resizeFlag=0;
+	      $(window).trigger('resized');
+	    }
+	    setTimeout(function(){
+	      checkSizeChange();
+	    }, 200);
+	}
+
 
 $(document).ready(function(){
 
@@ -19,32 +35,158 @@ $(document).ready(function(){
 
 
 	/*++++++++++++++++++++++++++++++++++++
-		slidepage
+		sidebar
 	++++++++++++++++++++++++++++++++++++++*/
-	var SidebarAnim = new TimelineLite({paused:true});
-	SidebarAnim
-		.to($(".social-icons, #main-nav"),0.2,{left:0})
-		.to($("#main"),0.2,{left:250,right:"-=250"},"-=0.2");
-	
+	var sideS, sidebar = {
+		settings : {
+			$side : $("#sidebar"),
+			$main : $("#main"),
+			$trigger : $("a.mobilemenu"),
+			$totaltrigger : $(".social-icons, #main-nav, #main"),
+			$sideFooter:$('#sidebar-footer'),
+			$sideContent:$('#main-nav'),
+			$sidebarWrapper:$('#sidebar-wrapper'),
+			windowWidth:$(window).width()
+			
+		},
 
-	$("a.mobilemenu").on("click",function(){
-		SidebarAnim.play();
-	});
-	$(".social-icons, #main-nav, #main").on("click",function(){
-		SidebarAnim.reverse();		
-	});
+		init : function(){
+			sideS = this.settings;
+			sideS.contentPadding=parseInt(sideS.$sideContent.css('padding-bottom'));
+			this.customScrollFlag=false;
+			this.setScrollBar();
+			this.setContentPadding();
+			this.bindUiActions();
+			this.setMobileSide();
+		},
 
+		isIn : function(){
+			return (sideS.$main.hasClass("sideIn"));
+		},
 
-	/*++++++++++++++++++++++++++++++++++++++++++++++
-		custom scrolls with perfectScroll plugin
-	++++++++++++++++++++++++++++++++++++++++++++++++*/
-	$("#main-nav").perfectScrollbar({
-		wheelPropagation:true,
-		wheelSpeed:80
-	});
+		bindUiActions : function(){
+			var self = this;
+			sideS.$trigger.click(function(e){
+				e.preventDefault();
+				if (self.isIn()){
+					self.sideOut();
+				}else{
+					self.sideIn();
+				}
+			});
+			sideS.$totaltrigger.click(function(){
+				if ($(window).width() < 960 && self.isIn())
+					self.sideOut(); 
+			});
 
-	
-	
+			sideS.$side.on('afterSlideIn',function(){
+				if($(window).width()<=600){// under 600px sidebar will cover the whole screen
+					sideS.$main.css('display','none');
+				}
+			});
+
+			sideS.$side.on('beforeSlideOut',function(){
+				if($(window).width()<=600){
+					sideS.$main.css('display','block');
+				}
+			});
+
+			$(window).on('resized',function(){
+				sideS.windowWidth=$(window).width();
+				self.setContentPadding();
+				self.setScrollBar();
+				if ( sideS.windowWidth > 991 ){
+					self.reset();
+				}else{
+					self.gomobile();
+				}
+
+				self.setMobileSide();
+			});
+		},
+
+		sideIn : function() {
+			var self = this;
+			var sWidth=sideS.$side.width();
+			sideS.$side.trigger('beforeSlideIn');
+			var SidebarAnimIn = new TimelineLite({onComplete:function(){
+				sideS.$side.trigger('afterSlideIn');
+			}});
+			SidebarAnimIn.to(sideS.$main,0.5,{left:sWidth,right:-sWidth,ease:Power2.easeIn},"-=0.2");
+			sideS.$main.addClass('sideIn');
+		},
+
+		sideOut : function(){
+			var self = this;
+			sideS.$side.trigger('beforeSlideOut');
+			var SidebarAnimOut = new TimelineLite({onComplete:function(){
+				sideS.$side.trigger('afterSlideOut');
+			}});
+			SidebarAnimOut
+				//.to(sideS.$side,0.2,{left:-250})
+				.to(sideS.$main,0.5,{left:0,right:0,ease:Power2.easeIn},"-=0.2");
+			sideS.$main.removeClass('sideIn');
+		},
+
+		reset : function(){
+			sideS.$main.css({left:250, right:0});
+			sideS.$side.css({left:0});
+			sideS.$main.addClass('sideIn');
+		},
+
+		gomobile : function (){
+			sideS.$main.css({left:0, right:0});
+			sideS.$side.css({left:0});	
+			sideS.$main.removeClass('sideIn');
+		},
+		setMobileSide:function(){
+			var self=this;
+
+			var tWidth=$(window).width();
+			
+			if (tWidth<600){
+				sideS.$side.width(tWidth);
+			}else{
+				sideS.$side.width('');
+				sideS.$main.css('display','block');
+			}
+			
+		},
+		setContentPadding:function(){
+			var self=this;
+			var footerHeight=sideS.$sideFooter.outerHeight();
+			sideS.$sideContent.css({paddingBottom:sideS.contentPadding+footerHeight});
+			
+		},
+		setScrollBar:function(){
+			var self=this;
+
+			if ((sideS.windowWidth>1024 || !isTouchSupported()) && !self.customScrollFlag){//Considiton that we want custom scrollbar
+				self.setCustomScroll();
+			}else if(sideS.windowWidth<=1024 && isTouchSupported() && self.customScrollFlag){ // Condition that we don't want custom scrollbar
+				self.destroyCustomScroll();
+			}
+			
+		},
+		setCustomScroll:function(){
+
+			this.customScrollFlag=true;
+			sideS.$sidebarWrapper.niceScroll({
+				horizrailenabled:false,
+				cursorwidth: '6px',
+			    cursorborder: 'none',
+			    cursorborderradius:'0px',
+			    cursorcolor:"#aaa"
+			});
+		
+		},destroyCustomScroll:function(){
+			sideS.$sidebarWrapper.niceScroll('destroy');
+			this.customScrollFlag=false;
+
+		}
+
+	}
+	sidebar.init();
 
 	/*++++++++++++++++++++++++++++++++++++
 		pages 
@@ -123,7 +265,7 @@ $(document).ready(function(){
 			( new TimelineLite() )
 				.set(gotop2,{ left:"100%",zIndex:self.topz})
 				.set(gotop3,{zIndex:maxz+1})
-				.to(gotop2,0.4,{left:"0", width:"100%"})
+				.to(gotop2,0.4,{left:"15%"})
 				.to(gotop3,0.3,{ left:0 , onComplete:function(){gotop3.removeClass('currentpage');} },"-=0.2");
 		},
 
@@ -137,7 +279,7 @@ $(document).ready(function(){
 
 			(new TimelineLite())
 				.set(gotop2,{zIndex:self.topz-1})
-				.to(gotop2,0.4,{left:"0"})
+				.to(gotop2,0.4,{left:"15%"})
 				.to(gotop1,0.5,
 					{
 						left:"100%",
@@ -236,7 +378,7 @@ $(document).ready(function(){
 		$car.carouFredSel({
 			circular: true,
 			infinite: true,
-			width: '118%',
+			width: '100%',
 			height: 250,
 			items: {
 				visible:3,
@@ -357,14 +499,18 @@ $(document).ready(function(){
 		mainClass: 'my-mfp-slide-bottom'
 	});
 
-	/*++++++++++++++++++++++++++++++++++++
-		stellar for contact page backgrounds
-	++++++++++++++++++++++++++++++++++++++*/
-	$(".stellar").stellar({
-  		verticalOffset: -100,
-  		horizontalScrolling: false,
-	});
+	/* Detect touch devices*/
+	function isTouchSupported(){
+        //check if device supports touch
+        var msTouchEnabled = window.navigator.msMaxTouchPoints;
+        var generalTouchEnabled = "ontouchstart" in document.createElement("div");
+     
+        if (msTouchEnabled || generalTouchEnabled) {
+            return true;
+        }
+        return false;
 
+  	}
 
 });
 
